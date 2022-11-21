c6f7ee30313a13e28322b2f616f59c1ae8f75895
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2022-11-22 01:46:32 +0800

    luci：添加配置文件路径

--- luci-app-lucky/luasrc/model/cbi/lucky.lua
+++ luci-app-lucky/luasrc/model/cbi/lucky.lua
@@ -2,7 +2,9 @@
 
 local m, s ,o
 
-local m = Map("lucky", translate("Lucky"), translate("Main functions of Lucky: dynamic domain name ddns-go service, which replaces socat. It is mainly used for public IPv6 tcp/udp to intranet ipv4, http/https reverse proxy frp, and the default login username and password are 666")..translate("</br>For specific usage, see:")..translate("<a href=\'https://github.com/sirpdboy/luci-app-lucky.git' target=\'_blank\'>GitHub @sirpdboy/luci-app-lucky </a>") )
+m = Map("lucky")
+m.title = translate("Lucky")
+m.description = translate("Main functions of Lucky: dynamic domain name ddns-go service, which replaces socat. It is mainly used for public IPv6 tcp/udp to intranet ipv4, http/https reverse proxy frp, and the default login username and password are 666.")..translate("</br>For specific usage, see:")..translate("<a href=\'https://github.com/sirpdboy/luci-app-lucky.git' target=\'_blank\'>GitHub @sirpdboy/luci-app-lucky </a>。")
 
 m:section(SimpleSection).template  = "lucky_status"
 
@@ -10,12 +12,16 @@ s = m:section(TypedSection, "lucky", translate("Global Settings"))
 s.addremove=false
 s.anonymous=true
 
-o=s:option(Flag,"enabled",translate("Enable"))
+o = s:option(Flag,"enabled",translate("Enable"))
 o.default=0
 
-o=s:option(Value, "port",translate("Set the Lucky access port"))
-o.datatype="uinteger"
-o.default=16601
+o = s:option(Value, "port",translate("Set the Lucky access port"))
+o.datatype = "uinteger"
+o.default = 16601
+
+o = s:option(Value, "config", translate("Lucky configuration directory"))
+o.rmempty = '/etc/lucky'
+o.rmempty = false;
 
 m.apply_on_parse = true
 m.on_after_apply = function(self,map)
