a2e773c21c16b04ee09bbb9a45f29f0c1547dd5b
SirPdboy <63764630+sirpdboy@users.noreply.github.com>
2022-11-25 13:26:05 +0800

    升级1.0.4修复安全URL无效问题，增加用户名和密码显示功能

--- luci-app-lucky/luasrc/model/cbi/lucky.lua
+++ luci-app-lucky/luasrc/model/cbi/lucky.lua
@@ -4,7 +4,7 @@ local m, s ,o
 
 m = Map("lucky")
 m.title = translate("Lucky")
-m.description = translate("Main functions of Lucky: dynamic domain name ddns-go service, which replaces socat. It is mainly used for public IPv6 tcp/udp to intranet ipv4, http/https reverse proxy frp, and the default login username and password are 666.")..translate("</br>For specific usage, see:")..translate("<a href=\'https://github.com/sirpdboy/luci-app-lucky.git' target=\'_blank\'>GitHub @sirpdboy/luci-app-lucky </a>。")
+m.description = translate("Main functions of Lucky: dynamic domain name ddns-go service, which replaces socat. It is mainly used for public IPv6 tcp/udp to intranet ipv4, http/https reverse proxy frp")..translate("</br>For specific usage, see:")..translate("<a href=\'https://github.com/sirpdboy/luci-app-lucky.git' target=\'_blank\'>GitHub @sirpdboy/luci-app-lucky </a>。")
 
 m:section(SimpleSection).template  = "lucky_status"
 
@@ -21,6 +21,10 @@ o.default = 16601
 
 o = s:option(Flag, "AllowInternetaccess", translate("Enable Internet access"))
 o.default=0
+
+o = s:option(Button, "lucky_admin", translate("View Password"))
+o.rawhtml = true
+o.template = "lucky_admin"
 
 m.apply_on_parse = true
 m.on_after_apply = function(self,map)
